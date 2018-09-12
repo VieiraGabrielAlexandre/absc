6a1b4cbea10563f25d5f4eb09b8cf7bf2e3e64ee
Gabriel Vieira <gabrielvieira840@gmail.com>
2018-09-12 20:31:52 -0300

    cad label

--- cadastrarinstituicao.php
+++ cadastrarinstituicao.php
@@ -50,7 +50,7 @@ http://www.templatemo.com/tm-496-pipeline
 					<input type="hidden" name="classcliente" value="2">
 					<input type="hidden" name="situacao" value="1"><br>
 					<label>Data do cadastro</label>
-                    <input type="text" name="dataatual" value="<?php echo date('Y-m-d h:m:s');?>" />
+                    <label>Data do cadastro = <?php echo date('Y-m-d hh:mm:ss');?></label>
                     <br>				
 					<button type="submit">Efetuar Cadastro</button>
                 </form>
