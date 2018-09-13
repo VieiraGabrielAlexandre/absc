a965cc3aa9f54ae2b491dd53cdce1fe03ca5d7bf
Gabriel Vieira <gabrielvieira840@gmail.com>
2018-09-12 21:46:24 -0300

    teste

--- efetuarcadastro.php
+++ efetuarcadastro.php
@@ -6,7 +6,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" script language="JavaScript" type="text/javascript" src="js/MascaraValidacao.js"></script> 
     <title>Abrace Sua Causa s2</title>
 <!--
 Pipeline
