3ed4ad3b7d525c56c4d5c6518b9292f6b3a58ffd
marcelokvr96 <marcelokvr96@gmail.com>
2018-09-19 23:15:06 +0000

    Updated cliente.php

--- cliente.php
+++ cliente.php
@@ -41,8 +41,9 @@ http://www.templatemo.com/tm-496-pipeline
                         <ul class="tm-main-nav-ul"> 				
                         <?php
 					        session_start();
-                            echo "Bem Vindo";
-    				        echo "Usuario: ". $_SESSION['usuarioNome'];
+                            echo "Bem Vindo ";
+
+    				        echo . $_SESSION['usuarioNome'];
 				        ?>
 				        <br>
                             <li class="tm-nav-item"><a href="#welcome" class="tm-nav-item-link tm-button">
