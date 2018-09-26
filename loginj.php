0a9fa0ef28d5789302d93ef8843791acdc3c724f
gabrielvieira840 <gabrielvieira840@gmail.com>
2018-09-26 23:21:13 +0000

    Updated loginj.php

--- loginj.php
+++ loginj.php
@@ -51,7 +51,7 @@ http://www.templatemo.com/tm-496-pipeline
 					
 				</form>
 				
-				<form id="legenda">
+				<form id="legenda2">
 				<label> <a id="l1" href="login.php">Pessoa Fisica</a></label>
 				<label> <a id="l2" href="">esqueci minha senha</a></label>
 				</form>
